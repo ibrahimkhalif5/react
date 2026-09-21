1296a4bb2e6983d5ea1c6242447082227da263c8
IbrahimKhalif5 <ibrahimkhalif5@gmail.com>
2026-09-21 12:37:10 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from './Navbar'
+
+const App = () => {
+  return (
+    <>
+
+   <Navbar/>
+    
+    
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function navbar() {
+  return (
+    <Navbar expand="lg" bg="light" className="mb-3">
+      <Container>
+        <Navbar.Brand href="#home">Ibrahim</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#education">Education</Nav.Link>
+            <Nav.Link href="#achievements">Achievements</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default navbar;
