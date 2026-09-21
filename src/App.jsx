d96ed9959714bcfccf1ad6117b3f64cb812eab67
IbrahimKhalif5 <ibrahimkhalif5@gmail.com>
2026-09-21 13:24:07 -0400

    counter added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import Navbar from './Navbar'
 import Counter from './Counter'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <>
 
    <Navbar/>
+   <Home/>
    <Counter/>
     
     
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+  return (
+    <>
+    <p>Welcome to my portfolio website! I am a passionate web developer with a strong interest in creating innovative and user-friendly web applications. This website showcases my skills, projects, and experiences in the field of web development.</p>
+    
+    </>
+  )
+}
+
+export default Home
